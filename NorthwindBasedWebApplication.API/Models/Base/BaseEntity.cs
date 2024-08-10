2807e995c7bd37e1315eecb6ff04c037e6f86084
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-10 14:20:36 +0300

    IBaseEntityAndBaseEntityAdded

---
+++ NorthwindBasedWebApplication.API/Models/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Base
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ NorthwindBasedWebApplication.API/Models/Base/IBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Base
+{
+    public interface IBaseEntity
+    {
+        int Id { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+        DateTime? DeletedAt { get; set; }
+    }
+}
