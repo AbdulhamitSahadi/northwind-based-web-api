16360c72a2c261dc33680109acd6f80c9e4f0714
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:26:46 +0300

    EmployeeTerritoryJoinDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/EmployeeTerritory.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindBasedWebApplication.API.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class EmployeeTerritory : BaseEntity
+    {
+        //Navigation Properties
+
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+
+
+        [ForeignKey("Territory")]
+        public int TerritoryId { get; set; }
+        public Territory Territory { get; set; }
+    }
+}
