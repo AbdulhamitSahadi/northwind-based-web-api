bbee88683862a3c02c00fd66b1e110c92a76015b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:21:58 +0300

    TerritoryDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Territory.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using NorthwindBasedWebApplication.API.Models.Base;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class Territory : BaseEntity
+    {
+        [Required]
+        [Display(Name = "Territory Description")]
+        public string TerritoryDescription { get; set; }
+
+
+        [Display(Name = "PictureUrl")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigations
+        public ICollection<EmployeeTerritories> EmployeeTerritories { get; set; }
+
+
+        [Required]
+        [Display(Name = "Region")]
+        [ForeignKey("Region")]
+        public int RegionId { get; set; }
+        public Region Region { get; set; }
+
+    }
+}
