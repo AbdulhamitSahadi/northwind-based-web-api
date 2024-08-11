1407be69fb63764739a4adf0fdda0014850450c3
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:24:32 +0300

    CustomerDemographicDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/CustomerDemographic.cs
@@ -0,0 +1,20 @@
+﻿using NorthwindBasedWebApplication.API.Models.Base;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class CustomerDemographic : BaseEntity
+    {
+        [Display(Name = "Customer Description")]
+        public string? CustomerDescription { get; set; }
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigation properties
+        public ICollection<CustomerCustomerDemographic> CustomersCustomerDemographics { get; set; }
+    }
+}
