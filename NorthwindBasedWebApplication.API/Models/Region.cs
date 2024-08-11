4df54c5f1925b607e6514f66ae358b516c4d840b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:19:46 +0300

    RegionDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Region.cs
@@ -0,0 +1,20 @@
+﻿using NorthwindBasedWebApplication.API.Models.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class Region : BaseEntity
+    {
+        [Required(ErrorMessage = "Region description is required!")]
+        [Display(Name = "Region Description")]
+        public string RegionDescription { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+        //Navigation Properties
+        public ICollection<Territory> Territories { get; set; }
+    }
+}
