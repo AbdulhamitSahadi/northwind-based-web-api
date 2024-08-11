635801304c743d43ba2f59a792fdfa11d28012d9
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:12:20 +0300

    CategoryDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class Category
+    {
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "Category name is required!")]
+        public string CategoryName { get; set; }
+
+
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+
+        //Navigation Properties
+        public ICollection<Product>? Products { get; set; }
+    }
+}
