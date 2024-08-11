15f89eccfee8d6efe4d16ea44d08d777d0ac96cf
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:24:10 +0300

    CustomerCustomerDemographicJoinDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/CustomerCustomerDemographic.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindBasedWebApplication.API.Models.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class CustomerCustomerDemographic : BaseEntity
+    {
+        //Navigation Properties
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+
+        [ForeignKey("CustomerType")]
+        public int CustomerTypeId { get; set; }
+        public CustomerDemographic CustomerType { get; set; }
+    }
+}
