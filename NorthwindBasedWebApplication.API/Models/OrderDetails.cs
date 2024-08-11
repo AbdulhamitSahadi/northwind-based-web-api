026d105cc793490fe8a48d856031f2ee8ee3c1af
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-11 22:28:50 +0300

    OrderDetailsJoinDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/OrderDetails.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using NorthwindBasedWebApplication.API.Models.Base;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class OrderDetails : BaseEntity
+    {
+        [Display(Name = "Unit Price")]
+        [Required(ErrorMessage = "Unit Price is required field!")]
+        public decimal UnitPrice { get; set; }
+
+
+
+        [Display(Name = "Quantity")]
+        [Required(ErrorMessage = "Quantity is required field")]
+        public short Quantity { get; set; }
+
+
+
+        [Display(Name = "Discount")]
+        [Required(ErrorMessage = "Discount is required field!")]
+        public float Discount { get; set; }
+
+
+        //Navigation Properties 
+        [Required(ErrorMessage = "Order Id is required field!")]
+        [ForeignKey("Order")]
+        [Display(Name = "Order Id")]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+
+        [Display(Name = "Product Id")]
+        [Required(ErrorMessage = "Product Id is required field!")]
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+
+
+        public string? PictureUrl { get; set; }
+    }
+}
