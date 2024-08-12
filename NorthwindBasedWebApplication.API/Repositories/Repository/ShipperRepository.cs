9c7fb751cc4eeb9e86ad55eafbd2c8882f049cf2
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:56:43 +0300

    ShipperRepositoryModified

--- NorthwindBasedWebApplication.API/Repositories/Repository/ShipperRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/Repository/ShipperRepository.cs
@@ -1,4 +1,5 @@
-﻿using NorthwindBasedWebApplication.API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebApplication.API.Data;
 using NorthwindBasedWebApplication.API.Models;
 using NorthwindBasedWebApplication.API.Repositories.Base;
 using NorthwindBasedWebApplication.API.Repositories.IRepository;
@@ -14,6 +15,15 @@ namespace NorthwindBasedWebApplication.API.Repositories.Repository
         public ShipperRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        public async Task<ICollection<Order>> GetOrdersByShipper(int id)
+        {
+            var orders = await _context.Orders
+                .Where(i => i.ShipVia == id)
+                .ToListAsync();
+
+            return orders;
         }
     }
 }
