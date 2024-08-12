607069a81c04809b45e4f3401be6adac933e4969
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 11:32:19 +0300

    EmployeeDTOsAdded

---
+++ NorthwindBasedWebApplication.API/Models/DTOs/EmployeeDTOs/CreateEmployeeDto.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.EmployeeDTOs
+{
+    public class CreateEmployeeDto
+    {
+
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last name is required field!")]
+        public string LastName { get; set; }
+
+
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First name is required field!")]
+        public string FirstName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Title is required field!")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+
+        [Required(ErrorMessage = "Title of Courtesy is required field!")]
+        [Display(Name = "Title Of Courtesy")]
+        public string TitleOfCourtesy { get; set; }
+
+
+
+        [Required(ErrorMessage = "Birth date is required field!")]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+
+
+        [Required(ErrorMessage = "Hire Date is required field!")]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+
+
+        [Required(ErrorMessage = "Address is required field!")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+
+        [Required(ErrorMessage = "City is required field!")]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+
+
+        [Display(Name = "Region")]
+        public string? Region { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Postal Code is required field!")]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+
+        [Required(ErrorMessage = "Country is required field!")]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+
+        [Required(ErrorMessage = "Home phone is required field!")]
+        [Display(Name = "Home Phone")]
+        public string HomePhone { get; set; }
+
+
+
+        [Required(ErrorMessage = "Extension is required field!")]
+        [Display(Name = "Extension")]
+        public string? Extension { get; set; }
+
+
+        [Display(Name = "Photo")]
+        public string? PictureUrl { get; set; }
+
+
+        [Display(Name = "Notes")]
+        public string? Notes { get; set; }
+
+
+        [Display(Name = "Reports To")]
+        public int? ReportsTo { get; set; }
+       
+    }
+}
---
+++ NorthwindBasedWebApplication.API/Models/DTOs/EmployeeDTOs/ReadEmployeeDto.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.EmployeeDTOs
+{
+    public class ReadEmployeeDto
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+
+        [Display(Name = "Title Of Courtesy")]
+        public string TitleOfCourtesy { get; set; }
+
+
+
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+
+
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+
+
+        [Display(Name = "Region")]
+        public string? Region { get; set; }
+
+
+
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+
+
+
+        [Display(Name = "Home Phone")]
+        public string HomePhone { get; set; }
+
+
+
+        
+        [Display(Name = "Extension")]
+        public string? Extension { get; set; }
+
+
+
+        [Display(Name = "Photo")]
+        public string? PictureUrl { get; set; }
+
+
+
+        [Display(Name = "Notes")]
+        public string? Notes { get; set; }
+    }
+}
