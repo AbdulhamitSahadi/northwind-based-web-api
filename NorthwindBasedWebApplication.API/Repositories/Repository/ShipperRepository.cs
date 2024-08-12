283c3712f35ee0b54c3f69d4cfbbb31803185a1f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:55:59 +0300

    ShipperRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/ShipperRepository.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class ShipperRepository : EntityBaseRepository<Shipper>, IShipperRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public ShipperRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
