558e71c224c816fd45b2203e115453b052fcf8a8
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:55:46 +0300

    EmployeeRepositoryModified

--- NorthwindBasedWebApplication.API/Repositories/Repository/EmployeeRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/Repository/EmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using NorthwindBasedWebApplication.API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebApplication.API.Data;
 using NorthwindBasedWebApplication.API.Models;
 using NorthwindBasedWebApplication.API.Repositories.Base;
 using NorthwindBasedWebApplication.API.Repositories.IRepository;
@@ -13,6 +14,26 @@ namespace NorthwindBasedWebApplication.API.Repositories.Repository
         public EmployeeRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        public async Task<ICollection<Order>> GetOrdersByEmployeeAsync(int id)
+        {
+            var orders = await _context.Orders
+                .Where(i => i.EmployeeId == id)
+                .OrderBy(i => i.Id)
+                .ToListAsync();
+
+            return orders;
+        }
+
+        public async Task<ICollection<Territory>> GetTerritoriesByEmployeeAsync(int id)
+        {
+            var territories = await _context.EmployeesTerritories
+                .Where(i => i.EmployeeId == id)
+                .Select(t => t.Territory)
+                .ToListAsync();
+
+            return territories;
         }
     }
 }
