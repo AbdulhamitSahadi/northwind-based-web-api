93a19ff1ee0ceede5397393f1eb49beeb5c96e0b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:55:22 +0300

    ProductRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class ProductRepository : EntityBaseRepository<Product>, IProductRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
