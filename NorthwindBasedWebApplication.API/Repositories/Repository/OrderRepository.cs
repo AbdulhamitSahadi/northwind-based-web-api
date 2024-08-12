94db56a973f1d8b8842c5391ac0efcffe6228718
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:55:05 +0300

    OrderRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class OrderRepository : EntityBaseRepository<Order>, IOrderRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public OrderRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
