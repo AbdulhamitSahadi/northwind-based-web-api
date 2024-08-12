6b6fbd74b9d91cf8f9d37c9a2a3db3777b03f330
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 11:33:36 +0300

    SupplierDTOsAdded

---
+++ NorthwindBasedWebApplication.API/Models/DTOs/SupplierDTOs/CreateSupplierDto.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.SupplierDTOs
+{
+    public class CreateSupplierDto
+    {
+
+
+        [Required(ErrorMessage = "Company name is required")]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Contact message is required field!")]
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Contact title is required field!")]
+        [Display(Name = "Contact Title")]
+        public string ContactTitle { get; set; }
+
+
+
+        [Required(ErrorMessage = "Address is required field")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+
+        [Required(ErrorMessage = "City is required field!")]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+
+
+        [Display(Name = "Region")]
+        public string? Region { get; set; }
+
+
+        [Required(ErrorMessage = "Postal code is required field!")]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+
+
+        [Required(ErrorMessage = "Country is required field!")]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+
+
+        [Required(ErrorMessage = "Phone is required field!")]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+
+
+        [Display(Name = "Fax")]
+        public string? Fax { get; set; }
+
+
+        [Display(Name = "Home Page")]
+        public string? HomePage { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+
+     
+    }
+}
---
+++ NorthwindBasedWebApplication.API/Models/DTOs/SupplierDTOs/ReadSupplierDto.cs
@@ -0,0 +1,76 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.SupplierDTOs
+{
+    public class ReadSupplierDto
+    {
+
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+
+
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+
+
+ 
+        [Display(Name = "Contact Title")]
+        public string ContactTitle { get; set; }
+
+
+
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+
+
+        [Display(Name = "Region")]
+        public string? Region { get; set; }
+
+
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+
+
+   
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+
+
+ 
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+
+
+        [Display(Name = "Fax")]
+        public string? Fax { get; set; }
+
+
+        [Display(Name = "Home Page")]
+        public string? HomePage { get; set; }
+
+
+        [Display(Name = "Picture")]
+        public string? PictureUrl { get; set; }
+
+    }
+}
