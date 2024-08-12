07952d71d9cfeff9d44525764d5ace72daa08907
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:56:30 +0300

    TerritoryRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/TerritoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class TerritoryRepository : EntityBaseRepository<Territory>, ITerritoryRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TerritoryRepository(ApplicationDbContext context) : base (context)
+        {
+            _context = context;
+        }
+    }
+}
