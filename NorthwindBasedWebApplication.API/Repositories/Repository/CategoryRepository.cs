8b662bfff405837a33d0c4701df83056403ff5dd
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:49:20 +0300

    CategoryRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class CategoryRepository : EntityBaseRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Product>> GetProductsByCategory(int id)
+        {
+            var products = await _context.Products
+                .Where(i => i.CategoryId == id)
+                .OrderBy(o => o.Id)
+                .ToListAsync();
+
+            return products;
+        }
+    }
+}
