72f16d05260910d6146ef6629f9e1b4f478a8f90
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:57:20 +0300

    CategoryDTOsAdded

---
+++ NorthwindBasedWebApplication.API/Models/DTOs/CategoryDTOs/CreateCategoryDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.CategoryDTOs
+{
+    public class CreateCategoryDto
+    {
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "Category name is required!")]
+        public string CategoryName { get; set; }
+
+
+
+        [Display(Name = "Description")]
+        public string? Description { get; set; }
+
+
+
+        [Display(Name = "Picture")]
+
+        public string? PictureUrl { get; set; }
+    }
+}
---
+++ NorthwindBasedWebApplication.API/Models/DTOs/CategoryDTOs/ReadCategoryDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthwindBasedWebApplication.API.Models.DTOs.CategoryDTOs
+{
+    public class ReadCategoryDto
+    {
+
+        [Display(Name = "Category Id")]
+        public int Id { get; set; }
+
+
+
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "Category name is required field!")]
+
+        public string CategoryName { get; set; }
+
+
+
+        [Display(Name = "Description")]
+
+        public string? Description { get; set; }
+
+
+
+
+        [Display(Name = "Picture")]
+
+        public string? PictureUrl { get; set; }
+    }
+}
