bb0a075657af10fd7ab289f5d8fc09f5d2a58dd2
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:57:58 +0300

    ApiResponseModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class ApiResponse
+    {
+
+        public ApiResponse()
+        {
+            ErrorMessages = new List<string>();
+            data = null;
+        }
+
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; }
+        public object data { get; set; }
+    }
+}
