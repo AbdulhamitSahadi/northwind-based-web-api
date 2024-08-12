fa343a8f47255278ffe24511cf3ae7db8f62658f
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:54:14 +0300

    CustomerDemographicRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/CustomerDemographicRepository.cs
@@ -0,0 +1,17 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class CustomerDemographicRepository : EntityBaseRepository<CustomerDemographic>, ICustomerDemographicRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerDemographicRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
