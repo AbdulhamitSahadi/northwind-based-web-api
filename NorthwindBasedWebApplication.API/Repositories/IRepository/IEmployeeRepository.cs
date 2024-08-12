91665002cce93d8562f7adfe6f14c41150f8d3c8
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:52:31 +0300

    IEmployeeRepositoryModefied

--- NorthwindBasedWebApplication.API/Repositories/IRepository/IEmployeeRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/IEmployeeRepository.cs
@@ -5,5 +5,7 @@ namespace NorthwindBasedWebApplication.API.Repositories.IRepository
 {
     public interface IEmployeeRepository : IEntityBaseRepository<Employee>
     {
+        Task<ICollection<Territory>> GetTerritoriesByEmployeeAsync(int id);
+        Task<ICollection<Order>> GetOrdersByEmployeeAsync(int id);
     }
 }
