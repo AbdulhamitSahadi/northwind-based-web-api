d7163da0034b425d75ff5846b7fbb873d002579b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:54:18 +0300

    ITerritoryRepositoryModified

--- NorthwindBasedWebApplication.API/Repositories/IRepository/ITerritoryRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/ITerritoryRepository.cs
@@ -5,5 +5,7 @@ namespace NorthwindBasedWebApplication.API.Repositories.IRepository
 {
     public interface ITerritoryRepository : IEntityBaseRepository<Territory>
     {
+        Task<ICollection<Employee>> GetEmployeesByTerritoryAsync(int id);
+        Task<Region> GetRegionByTerritoryAsync(int id);
     }
 }
