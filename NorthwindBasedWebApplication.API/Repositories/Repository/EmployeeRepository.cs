3bd9516ee3c6c4874906e215a5d10a4dedaaf1b5
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 09:54:50 +0300

    EmployeeRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class EmployeeRepository : EntityBaseRepository<Employee>, IEmployeeRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
