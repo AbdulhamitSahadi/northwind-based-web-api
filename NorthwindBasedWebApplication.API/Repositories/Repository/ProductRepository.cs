fae8fad39cc86b805f2d2075d180f237360fee36
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-12 13:56:06 +0300

    ProductRepositoryModified

--- NorthwindBasedWebApplication.API/Repositories/Repository/ProductRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using NorthwindBasedWebApplication.API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBasedWebApplication.API.Data;
 using NorthwindBasedWebApplication.API.Models;
 using NorthwindBasedWebApplication.API.Repositories.Base;
 using NorthwindBasedWebApplication.API.Repositories.IRepository;
@@ -13,6 +14,36 @@ namespace NorthwindBasedWebApplication.API.Repositories.Repository
         public ProductRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        public async Task<Category> GetCategoryByProductAsync(int id)
+        {
+            var category = await _context.Products
+                .Where(i => i.Id == id)
+                .Select(c => c.Category)
+                .FirstOrDefaultAsync();
+
+            return category;
+        }
+
+        public async Task<ICollection<Order>> GetOrdersByProductAsync(int id)
+        {
+            var orders = await _context.OrderDetails
+                .Where(i => i.ProductId == id)
+                .Select(o => o.Order)
+                .ToListAsync();
+
+            return orders;
+        }
+
+        public async Task<Supplier> GetSupplierByProductAsync(int id)
+        {
+            var supplier = await _context.Products
+                .Where(i => i.Id == id)
+                .Select(s => s.Supplier)
+                .FirstOrDefaultAsync();
+
+            return supplier;
         }
     }
 }
