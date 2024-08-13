888a26ef61f5969c7e3ecc905b6ad3a2f2a119db
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-13 14:59:51 +0300

    ApplicationUserDomainModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? UserIdentification { get; set; }
+    }
+}
