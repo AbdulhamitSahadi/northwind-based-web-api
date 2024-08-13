bc9d68bc04379e5746a5fb8719a87e467cc9a92e
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-13 14:58:14 +0300

    IUserRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using NorthwindBasedWebApplication.API.Models.DTOs.AuthDTOs;
+using NorthwindBasedWebApplication.API.Models.DTOs.UserDTOs;
+
+namespace NorthwindBasedWebApplication.API.Repositories.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<bool> IsUniqueUser(UserDto user);
+        Task<RegisterResponseDto> Register(RegisterRequestDto registerRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
