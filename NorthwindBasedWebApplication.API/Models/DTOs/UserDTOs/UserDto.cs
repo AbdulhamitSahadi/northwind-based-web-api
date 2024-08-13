a24bc292b849e9318e6c9de917af6e3cea90b3a1
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-13 15:00:31 +0300

    UserDtoAdded

---
+++ NorthwindBasedWebApplication.API/Models/DTOs/UserDTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.DTOs.UserDTOs
+{
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
