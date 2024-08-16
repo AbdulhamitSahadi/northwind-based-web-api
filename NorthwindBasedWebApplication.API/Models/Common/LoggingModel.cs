567fc84e1641c8800b9712937d457303a9d758ad
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-16 17:01:09 +0300

    LoggingModelsAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/LoggingModel.cs
@@ -0,0 +1,16 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class LoggingModel
+    {
+        public string Exception { get; set; }
+        public string MethodType { get; set; }
+        public string User { get; set; }
+        public string Role { get; set; }
+        public string Details { get; set; }
+        public string Data { get; set; }
+        public bool Success { get; set; }
+        public bool Failed { get; set; }
+        public string ErrorMessage { get; set; }
+        public string StatusCode { get; set; }
+    }
+}
---
+++ NorthwindBasedWebApplication.API/Models/Common/LoggingModelBuilder.cs
@@ -0,0 +1,80 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class LoggingModelBuilder
+    {
+        private readonly LoggingModel _logModel;
+
+        public LoggingModelBuilder SetException(string exception)
+        {
+            _logModel.Exception = exception;
+            return this;
+        }
+
+
+        public LoggingModelBuilder SetMethodType(string methodType)
+        {
+            _logModel.MethodType = methodType;
+            return this;
+        }
+
+
+        public LoggingModelBuilder SetUser(string user)
+        {
+            _logModel.User = user;
+            return this;
+        }
+
+
+        public LoggingModelBuilder SetRole(string role)
+        {
+            _logModel.Role = role;
+            return this;
+        }
+
+        public LoggingModelBuilder SetDetails(string details)
+        {
+            _logModel.Details = details;
+            return this;
+        }
+
+
+        public LoggingModelBuilder SetData(string data)
+        {
+            _logModel.Data = data;
+            return this;
+        }
+
+
+        public LoggingModelBuilder SetSuccess()
+        {
+            _logModel.Success = true;
+            return this;     
+        }
+
+
+        public LoggingModelBuilder SetFailed()
+        {
+            _logModel.Failed = false;
+            return this;
+        }                   
+
+
+        public LoggingModelBuilder SetErrorMessage(string errorMessage)
+        {
+            _logModel.ErrorMessage = errorMessage;
+            return this;    
+        }
+
+
+        public LoggingModelBuilder SetStatusCode(string statusCode)
+        {
+            _logModel.StatusCode = statusCode;
+            return this;
+        }
+
+        public LoggingModel Build()
+        {
+            return _logModel;
+        }
+    }
+}
