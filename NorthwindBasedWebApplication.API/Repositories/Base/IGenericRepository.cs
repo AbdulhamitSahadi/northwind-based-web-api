9d20087e4fce0ee1ea47c81d79708fa2f9dd56ef
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-19 22:28:21 +0300

    IGenericRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Base/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Base
+{
+    public interface IGenericRepository<T> where T : IdentityRole<int>
+    {
+        IDbContextTransaction Transaction();
+
+        void Commit();
+        void RollBack();
+
+        IQueryable<T> GetTableAsTracked();
+        IQueryable<T> GetTableAsNoTracked();
+
+        Task<bool> SaveChangesAsync();
+    }
+}
