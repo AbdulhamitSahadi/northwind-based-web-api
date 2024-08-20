81261cd7039554a27f48a339b11038200a555362
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:13:15 +0300

    RoleDtoAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/RoleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class RoleDto
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+
+        
+    }
+}
