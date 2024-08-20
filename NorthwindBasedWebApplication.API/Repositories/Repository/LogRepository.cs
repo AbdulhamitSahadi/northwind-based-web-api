bed66b1370e0f79c2fc87447760b86980d19cffa
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:11:12 +0300

    LogRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/Repository/LogRepository.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindBasedWebApplication.API.Data;
+using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+using NorthwindBasedWebApplication.API.Repositories.IRepository;
+
+namespace NorthwindBasedWebApplication.API.Repositories.Repository
+{
+    public class LogRepository : EntityBaseRepository<LogDto>, ILogRepository
+    {
+        private readonly ApplicationDbContext _context; 
+
+
+        public LogRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
