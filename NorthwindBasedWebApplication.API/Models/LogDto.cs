2c3977e6bd645ea208eb781afc07541ceb875a6b
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:14:58 +0300

    LogDtoAdded

---
+++ NorthwindBasedWebApplication.API/Models/LogDto.cs
@@ -0,0 +1,20 @@
+﻿using NorthwindBasedWebApplication.API.Models.Base;
+
+namespace NorthwindBasedWebApplication.API.Models
+{
+    public class LogDto : BaseEntity
+    {
+        public string Message { get; set; }
+        public string Level { get; set; }
+        public string Timestamp { get; set; }
+        public string MethodType { get; set; }
+        public string User { get; set; }
+        public string Role { get; set; }
+        public string Details { get; set; }
+        public string StatusCode { get; set; }
+        public bool Success { get; set; }
+        public bool Failed { get; set; }
+        public string? ErrorMessage { get; set; }
+       
+    }
+}
