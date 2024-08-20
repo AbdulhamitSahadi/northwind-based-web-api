92fc010a04a5c6f17ebc1fd17b3e14cd2ffadff6
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:13:51 +0300

    UserClaimsReponseAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/UserClaimsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class UserClaimsResponse
+    {
+        public int UserId { get; set; }
+        public List<UserClaims> Claims { get; set; }
+    }
+}
