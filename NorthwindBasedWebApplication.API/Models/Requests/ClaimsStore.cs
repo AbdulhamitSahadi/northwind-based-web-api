9c61d7dea2b13a507fd552c651a53a2b08a97aec
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:11:31 +0300

    ClaimsStoreSeedAdded

---
+++ NorthwindBasedWebApplication.API/Models/Requests/ClaimsStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace NorthwindBasedWebApplication.API.Models.Requests
+{
+    public static class ClaimsStore
+    {
+        public static List<Claim> claims = new()
+        {
+            new Claim("Create Product", "false"),
+            new Claim("Update Product", "false"),
+            new Claim("Delete Product", "false")
+        };
+    }
+}
