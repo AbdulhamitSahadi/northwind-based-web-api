476e233e52c8d583c32a43782aadb70016529608
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:13:37 +0300

    UserRoleModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool HasRole { get; set; }
+    }
+}
