78c0a5dde6f8606b08bd76b7a55c65be757f341d
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:10:36 +0300

    IAuthenticationRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/IAuthenticationRepository.cs
@@ -0,0 +1,13 @@
+﻿using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Models.DTOs.AuthDTOs;
+using NorthwindBasedWebApplication.API.Models.DTOs.UserDTOs;
+
+namespace NorthwindBasedWebApplication.API.Repositories.IRepository
+{
+    public interface IAuthenticationRepository
+    {
+        Task<bool> IsUniqueUser(UserDto user);
+        Task<RegisterResponseDto> Register(RegisterRequestDto registerRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
