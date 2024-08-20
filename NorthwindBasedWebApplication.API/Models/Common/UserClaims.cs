51032a545f4754959e858be6c91da3004ebb0d3a
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:13:28 +0300

    UserClaimsModelAdded

---
+++ NorthwindBasedWebApplication.API/Models/Common/UserClaims.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthwindBasedWebApplication.API.Models.Common
+{
+    public class UserClaims
+    {
+        public string Type { get; set; }
+        public bool Value { get; set; }
+    }
+}
