9a7dca1aa5b1caf0ab4d3d89e4c6edd89afa48a0
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:12:04 +0300

    IAuthorizationRepositoryModified

--- NorthwindBasedWebApplication.API/Repositories/IRepository/IAuthorizationRepository.cs
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/IAuthorizationRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace NorthwindBasedWebApplication.API.Repositories.IRepository
+﻿using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Models.Common;
+using NorthwindBasedWebApplication.API.Models.Common.Authorization;
+using NorthwindBasedWebApplication.API.Models.Requests;
+
+
+namespace NorthwindBasedWebApplication.API.Repositories.IRepository
 {
     public interface IAuthorizationRepository
     {
+        Task<bool> AddRoleAsync(string roleName);
+        Task<bool> UpdateRoleAsync(Models.Common.Authorization.UpdateRoleRequest updateRoleRequest);
+        Task<bool> DeleteRoleAsync(int id);
+        Task<IReadOnlyList<RoleDto>> GetAllAsync();
+        Task<ApplicationRole> GetByIdAsync(int id);
+        Task<bool> IsExistsAsync(string role);
+        Task<bool> IsExistsAsync(int id);
+
+        Task<UserRolesResponse> GetRolesByUser(int id);
+        Task<UserClaimsResponse> GetClaimsByUser(int id);
+        
     }
 }
