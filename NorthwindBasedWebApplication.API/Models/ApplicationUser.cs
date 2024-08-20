120d0f3e628169080d8e2936b8c58c5eefc400fb
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:15:11 +0300

    ApplicationUserDomainModelModified

--- NorthwindBasedWebApplication.API/Models/ApplicationUser.cs
+++ NorthwindBasedWebApplication.API/Models/ApplicationUser.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using EntityFrameworkCore.EncryptColumn.Attribute;
 
 namespace NorthwindBasedWebApplication.API.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<int>
     {
         public string? UserIdentification { get; set; }
+        public string? FullName { get; set; }
+        public string? Address { get; set; }
+        public string? Country { get; set; }       
     }
 }
