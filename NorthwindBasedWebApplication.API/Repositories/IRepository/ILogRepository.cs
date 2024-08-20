3ee45a71727f21094e5a3c54bbcd2a3c86293afd
Abdulhamit <sahadi.abdulhamit@gmail.com>
2024-08-20 17:11:04 +0300

    ILogRepositoryAdded

---
+++ NorthwindBasedWebApplication.API/Repositories/IRepository/ILogRepository.cs
@@ -0,0 +1,10 @@
+﻿using NorthwindBasedWebApplication.API.Models;
+using NorthwindBasedWebApplication.API.Repositories.Base;
+
+namespace NorthwindBasedWebApplication.API.Repositories.IRepository
+{
+    public interface ILogRepository : IEntityBaseRepository<LogDto>
+    {
+        
+    }
+}
